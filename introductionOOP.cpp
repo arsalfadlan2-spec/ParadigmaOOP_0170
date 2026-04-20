42a9b81d7015b72cd3f1d92d9d6ff33968055f0a
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-04-20 14:32:35 +0700

    menambahkan class dan method

---
+++ introductionOOP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa{
+    float nilai;
+
+    void prinData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+};
+
